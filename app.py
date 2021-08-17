e68e62acf22401210fc822b364a2c324a81fe3da
LisaRebecca <lisaaschm@gmail.com>
2021-08-17 12:01:43 +0200

    sample-api:1.1 snapshot

--- app.py
+++ app.py
@@ -5,51 +5,13 @@ import os
 
 app = Flask(__name__)
 
-def get_db():
-    client = MongoClient(host='test_mongodb',
-                         port=27017, 
-                         username='root', 
-                         password='pass',
-                         authSource="admin")
-    db = client["animal_db"]
-    return db
-
 @app.route('/version')
 def get_version():
-    return "Version: 1.0"
-
-@app.route('/world')
-def get_world():
-    db=""
-    try:
-        db = get_db()
-        _world = db.world.find()
-        cities = [ { "_id" : city["id"], "city" : city["city"], "loc" : city["loc"], "pop" : city["pop"], "state" : city["state"] } for city in _world]
-        # return jsonify({"cities": db.world.find()})
-        return jsonify({"cities":str(_world)})
-    except:
-        pass
-    finally:
-        if type(db)==MongoClient:
-            db.close()
-
-@app.route('/animals')
-def get_stored_animals():
-    db=""
-    try:
-        db = get_db()
-        _animals = db.animal_tb.find()
-        animals = [{"id": animal["id"], "name": animal["name"], "type": animal["type"]} for animal in _animals]
-        return jsonify({"animals": animals})
-    except:
-        pass
-    finally:
-        if type(db)==MongoClient:
-            db.close()
+    return "Version: 1.1"
 
 @app.route('/')
 def hello_world():
-    return jsonify({"about" : "this is python flask running in a docker container."})
+    return "I am the sample API."
 
 if __name__ =='__main__':
     app.run(debug=True, host='0.0.0.0')
